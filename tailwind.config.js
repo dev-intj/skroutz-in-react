28ca4744a671c229de3cebaf1b9c45b3e7dbadaf
Christos Dematas <demataschristiano@gmail.com>
2023-12-17 03:26:17 +0200

    what is going with gh pages?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-radial":
+          "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        ocalla: ["Ocalla", "cursive"],
+        asher: ["Asher", "sans-serif"],
+        moki: ["Moki", "cursive"],
+        sticky: ["Sticky", "cursive"],
+        typold: ["Typold", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
